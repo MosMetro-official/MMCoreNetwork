5539cd13ea6779f299932a053ddfdfa5175b28d1
Гусейн Римиханов <rimikhanov-gr@mosmetro.ru>
2022-04-28 16:54:49 +0300

    naming ++

--- Package.swift
+++ Package.swift
@@ -21,11 +21,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CoreNetwork",
+            name: "MMCoreNetwork",
             dependencies: []),
         .testTarget(
-            name: "CoreNetworkTests",
-            dependencies: ["CoreNetwork"],
+            name: "MMCoreNetworkTests",
+            dependencies: ["MMCoreNetwork"],
             resources: [
                 .copy("Files/Tests.json")
             ]
